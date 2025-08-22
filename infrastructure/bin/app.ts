9df9ef41159c131d3d080c9446eb42f9f3bf4a92
nikhilyanamadala <yanamadala20@gmail.com>
2025-08-22 15:11:01 -0500

    fix dockerfile

--- infrastructure/bin/app.ts
+++ infrastructure/bin/app.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { PriceTrackerStack } from '../lib/price-tracker-stack';
 
 const app = new cdk.App();
-new PriceTrackerStack(app, 'HelloWorldStack', {
+new PriceTrackerStack(app, 'PriceTrackerStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
